cc0ccc310e31f5e0384cd8d7d3ec3be92c414e1d
efratgavri <e0548549517@gmail.com>
2023-11-29 14:26:17 +0200

    good Me

--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -6,8 +6,10 @@ main().catch(err => console.log(err));
 async function main() {
     mongoose.set('strictQuery' , false);
 
-    await mongoose.connect('mongodb://127.0.0.1:27017/maor23');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/maor23');
     // await mongoose.connect('mongodb+srv://{config.userDb}:{config.userPass}@cluster0.foomjir.mongodb.net/Maor24')
+    await mongoose.connect(`mongodb+srv://EfratGavriel:e213278765@cluster0.foomjir.mongodb.net/Efrat1234`);
+
     console.log("mongo connect started1111");
  
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
